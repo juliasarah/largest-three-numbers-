d390f323810cfe5b3652be5180206ce807ac5567
juliasarah <36396636+juliasarah@users.noreply.github.com>
2018-02-15 00:57:44 -0800

    Add files via upload

---
+++ largest three numbers.py
@@ -0,0 +1,18 @@
+num1 = 5
+
+num2 = 9
+
+num3 = 8
+ 
+
+if (num1 > num2) and (num1 > num3):
+
+   print("the largest number is",num1)
+
+elif (num2 > num1) and (num2 > num3):
+ 
+  print("the largest number is",num2)
+
+else:
+
+   print("the largest number is",num3)
